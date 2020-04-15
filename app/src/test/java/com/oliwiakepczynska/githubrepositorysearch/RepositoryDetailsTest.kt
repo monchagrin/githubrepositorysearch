98d9c2057db0a6cf69cb7c3cab9feb581832283c
DESKTOP-H793V9V\Oliwia <monchagrin@outlook.com>
2020-04-15 03:14:56 +0200

    adding test for repository details

---
+++ app/src/test/java/com/oliwiakepczynska/githubrepositorysearch/RepositoryDetailsTest.kt
@@ -0,0 +1,46 @@
+package com.oliwiakepczynska.githubrepositorysearch
+
+import com.oliwiakepczynska.githubrepositorysearch.repository.RepositoryDetails
+import io.reactivex.subjects.PublishSubject
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import retrofit2.HttpException
+import java.util.concurrent.CountDownLatch
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+
+class RepositoryDetailsTest {
+
+    private lateinit var sut: RepositoryDetails
+    private val mockWebServer = MockWebServer()
+    private val errorsStream = PublishSubject.create<Throwable>()
+
+    @Before
+    fun setup() {
+        mockWebServer.start()
+        sut = RepositoryDetails.create(NetworkClient.create(mockWebServer.url("/").toString()), errorsStream)
+    }
+
+    @After
+    fun tearDown() {
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    fun test_errors_stream_publishes_exception_when_error_occurs_on_http_request() {
+        val latch = CountDownLatch(2)
+        mockWebServer.enqueue(MockResponse().setResponseCode(500).setBody("[]"))
+        errorsStream.subscribe {
+            Assert.assertTrue(it is HttpException)
+            Assert.assertTrue((it as HttpException).code() == 500)
+            latch.countDown()
+        }
+        sut.getAllRepositories().subscribe {
+            Assert.assertTrue(it.isEmpty())
+            latch.countDown()
+        }
+        latch.await()
+    }
+}
