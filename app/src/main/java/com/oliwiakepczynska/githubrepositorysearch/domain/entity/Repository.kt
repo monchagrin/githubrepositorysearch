32233815933e3f0ea3e008c530506e1a9e0820aa
DESKTOP-H793V9V\Oliwia <monchagrin@outlook.com>
2020-04-21 03:52:46 +0200

    refactor of code

--- app/src/main/java/com/oliwiakepczynska/githubrepositorysearch/domain/entity/Repository.kt
+++ app/src/main/java/com/oliwiakepczynska/githubrepositorysearch/domain/entity/Repository.kt
@@ -1,51 +1,14 @@
 package com.oliwiakepczynska.githubrepositorysearch.domain.entity
 
-import android.os.Parcel
 import android.os.Parcelable
 import com.oliwiakepczynska.githubrepositorysearch.repository.RepositoryProprietor
+import kotlinx.android.parcel.Parcelize
 
-data class Repository(
+@Parcelize
+class Repository(
     val id: Int?,
     val name: String?,
-    val full_name: String?,
     val owner: RepositoryProprietor?,
     val private: Boolean,
-    val url: String?,
     val description: String?
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readParcelable(RepositoryProprietor::class.java.classLoader),
-        parcel.readByte() != 0.toByte(),
-        parcel.readString(),
-        parcel.readString()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeValue(id)
-        parcel.writeString(name)
-        parcel.writeString(full_name)
-        parcel.writeParcelable(owner, flags)
-        parcel.writeByte(if (private) 1 else 0)
-        parcel.writeString(url)
-        parcel.writeString(description)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Repository> {
-        override fun createFromParcel(parcel: Parcel): Repository {
-            return Repository(
-                parcel
-            )
-        }
-
-        override fun newArray(size: Int): Array<Repository?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
--- app/src/main/java/com/oliwiakepczynska/githubrepositorysearch/repository/RepositoryProprietor.kt
+++ app/src/main/java/com/oliwiakepczynska/githubrepositorysearch/repository/RepositoryProprietor.kt
@@ -1,39 +1,11 @@
 package com.oliwiakepczynska.githubrepositorysearch.repository
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
-data class RepositoryProprietor(
+@Parcelize
+class RepositoryProprietor(
     val login: String?,
     val id: Int?,
-    val avatar_url: String?,
-    val url: String?
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readString(),
-        parcel.readString()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(login)
-        parcel.writeValue(id)
-        parcel.writeString(avatar_url)
-        parcel.writeString(url)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<RepositoryProprietor> {
-        override fun createFromParcel(parcel: Parcel): RepositoryProprietor {
-            return RepositoryProprietor(parcel)
-        }
-
-        override fun newArray(size: Int): Array<RepositoryProprietor?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+    val avatar_url: String?
+) : Parcelable
